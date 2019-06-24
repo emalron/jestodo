478684173bc6c7c72042386d49b5f4f793787e63
Emalron <pjm2108@naver.com>
2019-06-24 19:24:57 +0900

    commit from WSL

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+
+window.onload = function() {
+    init();
+}
+
 function loadTemplate(id) {
     return document.getElementById(id).innerHTML;
 }
@@ -14,11 +19,35 @@ function replaceTemplate(template, data) {
     return result;
 }
 
-window.onload = function() {
-    
-    data = {who: "정민"};
-    var template = loadTemplate('hello');
-    var result = replaceTemplate(template, data);
-    
-    document.getElementById("display").innerHTML = result;
+function init() {
+    var head = [{name: "이름", title: "제목", date: "시간"}];
+    var head_id = "board-head";
+
+    var body = [];
+    body.push({name: "곰팡이", title: "What if I got CS degree?", date: "오늘"});
+    body.push({name: "Jes", title: "오늘 에기평 간사한테 드립칠뻔한 썰", date:"오늘"});
+    body.push({name: "Emily", title: "브붕콘 그만해 지겨워", date:"오늘"});
+    body.push({name: "Jes", title: "브붕콘 만들기 ver.2.0 끝!", date:"오늘"});
+    body.push({name: "Jes", title: "(죽음)", date:"어제"});
+    body.push({name: "Emily", title: "Jes 살아있니?", date:"어제"});
+    var body_id = "board-body";
+
+    put_data_into_table(head, head_id);
+    put_data_into_table(body, body_id);
+}
+
+function put_data_into_table(data, id) {
+    var table = document.getElementById(id);
+
+    console.log(table);
+
+    data.forEach(element => {
+        var template = loadTemplate('table');
+        result = replaceTemplate(template, element);
+
+        var row = document.createElement("tr");
+        console.log(row);
+        row.innerHTML = result;
+        table.appendChild(row);
+    });
 }
